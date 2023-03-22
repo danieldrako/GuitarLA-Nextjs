dec1eb0c91a728b74d25b206aeb0610c12518876
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-21 20:03:47 -0600

    after making the footer

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,29 @@
-
+import Link from "next/link"
+const styles = {}
 export default function Footer() {
   return (
-    <div>footer</div>
+    <footer>
+      <div className="contenedor">
+            <nav className={styles.navegacion} >
+                <Link href="/" >
+                    Inicio 
+                </Link>
+
+                <Link href="/nosotros" >
+                        Nosotros
+                </Link>
+
+                <Link href="/blog" >
+                        Blog
+                </Link>
+
+                <Link href="/tienda" >
+                        Tienda
+                </Link>
+            </nav>
+
+            <p> Todos los derechos reservados {new Date().getFullYear()}</p>
+      </div>
+    </footer>
   )
 }
