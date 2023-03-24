9841dc04f1d0927fc1c15b65e13a4ca62a960a60
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-24 09:43:17 -0600

    after dynamic routing with getStaticPaths and getStaticProps  (bug)

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -7,7 +7,23 @@ export default function Producto({guitarra}) {
   )
 }
 
-export async function getServerSideProps({query:{url}}){
+
+export async function getStaticPaths(){
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras`)
+    const {data} = await respuesta.json()
+    const paths = data.map(guitarra => ({
+        params: {
+            url: guitarra.attributes.url
+        }
+    }))
+
+    return {
+        paths,
+            fallback: false //next genera automaticamente la pagina de error
+    }
+}
+
+export async function getStaticProps({params:{url}}){
     const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
     const {data: guitarra} = await respuesta.json()
     return {
@@ -16,3 +32,13 @@ export async function getServerSideProps({query:{url}}){
         }
     }
 }
+
+// export async function getServerSideProps({query:{url}}){
+//     const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
+//     const {data: guitarra} = await respuesta.json()
+//     return {
+//         props: {
+//             guitarra
+//         }
+//     }
+// }
