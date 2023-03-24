1eb9e569397835d43d6391b9af6f6e9ab12b9555
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-24 09:05:06 -0600

    after dynamic routing (bug)

---
+++ pages/guitarras/[url].js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router"
+
+export default function Producto() {
+    const router = useRouter()
+    console.log(router)
+    
+  return (
+    <div>[url]</div>
+  )
+}
