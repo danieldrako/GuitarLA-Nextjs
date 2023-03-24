89751c90cefb2bde4aa8e99c5609ddbf634f0329
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-24 09:27:41 -0600

    after dynamic routing with getServerSideProps (bug)

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,10 +1,18 @@
-import { useRouter } from "next/router"
 
-export default function Producto() {
-    const router = useRouter()
-    console.log(router)
-    
+export default function Producto({guitarra}) {
+
+    console.log(guitarra[0].attributes.nombre)
   return (
     <div>[url]</div>
   )
+}
+
+export async function getServerSideProps({query:{url}}){
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
+    const {data: guitarra} = await respuesta.json()
+    return {
+        props: {
+            guitarra
+        }
+    }
 }
