400fb15b47ba5dea495c58a02f2a9a3d28fa5c5b
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-28 09:21:31 -0600

    show guitars and blog in the start page (bugwith cloudinary)

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,63 @@
 import Link from "next/link"
 import Layout from "@/components/layout"
+import Guitarra from "@/components/guitarra"
+import styles from '../styles/grid.module.css'
+import Post from "../components/post"
 
-export default function Home() {
+export default function Home({guitarras,posts}) {
   return (
     <>
       <Layout
         title={'Inicio'}
         description={'Blog de musica, venta de guitarras y más'}
       >
-        <h1>Hola Mundo de Next</h1>
+        <main className="contenedor">
+          <h1 className="heading">Nuestra Colección</h1>
+          <div className={styles.grid}>
+            {guitarras?.map(guitarra => (
+              <Guitarra
+                key={guitarra.id}
+                guitarra={guitarra.attributes}
+              />
+            ))}
+          </div>
+        </main>
+
+        <section className="contenedor">
+          <h2 className="heading">Blog</h2>
+          <div className={styles.grid}>
+            {posts?.map(post =>(
+              <Post
+                key={post.id}
+                post={post.attributes}
+              />
+            ))}
+          </div>
+        </section>
+
         <Link href = "/nosotros">Nosotros</Link>
       </Layout>
     </>
   )
+}
+
+
+export async function getStaticProps(){
+  const urlGuitarras = `${process.env.API_URL}/guitarras?populate=imagen`
+  const urlPosts = `${process.env.API_URL}/posts?populate=imagen`
+  const [resGuitarras, resPosts] = await Promise.all([
+    fetch(urlGuitarras),
+    fetch(urlPosts)
+  ])
+  
+  const [{data: guitarras},{data:posts}] = await Promise.all([
+    resGuitarras.json(),
+    resPosts.json()
+  ])
+  return {
+      props: {
+          guitarras,
+          posts
+      }
+  }
 }
