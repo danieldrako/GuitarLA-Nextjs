0b145676caac626a0de37d8adb09b236535290ae
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-30 16:00:22 -0600

    calculate the total on the purchase car (bug with cloudinary)

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,8 +1,17 @@
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Layout from "@/components/layout"
 import styles from '../styles/carrito.module.css'
 
 export default function Carrito({carrito, actualizarCantidad}) {
+
+    const [total, setTotal] = useState(0)
+
+    useEffect((first) => {
+        const calculoTotal = carrito.reduce((total,producto) => total + (producto.cantidad*producto.precio),0)
+        setTotal(calculoTotal)
+     },[carrito])
+
   return (
     <Layout title="Carrito de compras">
         <main className="contenedor">
@@ -51,7 +60,7 @@ export default function Carrito({carrito, actualizarCantidad}) {
 
                 <aside className={styles.resumen}>
                     <h3>Resumen del pedido</h3>
-                    <p>Total a pagar:</p>
+                    <p>Total a pagar: $ {total} </p>
                 </aside>
             </div>
         </main>
